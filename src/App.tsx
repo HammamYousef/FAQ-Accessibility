5cb928bda28fcc4eafbc70ea4bf461af9d180fbc
Hammam Yousef <h.abutaima@student.aaup.edu>
2025-07-18 23:08:48 +0300

    modify the images

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import FAQ_Card from "./components/FAQ_Card";
 
 const App = () => {
   return (
-    <main className="relative min-h-dvh flex flex-col items-center justify-center px-6 bg-purple-100 bg-[url('./background-pattern-mobile.svg')] sm:bg-[url('./background-pattern-desktop.svg')] bg-no-repeat bg-contain">
+    <main className="relative min-h-dvh flex flex-col items-center justify-center px-6 bg-purple-100 bg-[url('./assets/images/background-pattern-mobile.svg')] sm:bg-[url('./assets/images/background-pattern-desktop.svg')] bg-no-repeat bg-contain">
       <FAQ_Card />
       <div className="attribution">
-        Challenge by{" "}
+        Challenge by
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
         </a>
         . Coded by{" "}
-        <a href="https://www.frontendmentor.io/profile/HammamYousef" target="_blank">
+        <a
+          href="https://www.frontendmentor.io/profile/HammamYousef"
+          target="_blank"
+        >
           Hammam Yousef
         </a>
         .
